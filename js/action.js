edb25c03a022b0cf40e7bcd23f0282d446a69609
Zino Yue <zinoyue@gmail.com>
2013-05-19 22:47:03 +0800

    add new section add more effect add some functionality

--- js/action.js
+++ js/action.js
@@ -14,8 +14,27 @@
         obj.setIcon(icon);
     };
     nsd.user = {};
+    nsd.user.weibo = false;
     shared.gallery_id = 0;
-
+    shared.formatNumber = function(num) {
+        var str = String(Math.floor(num));
+        if (str.length <= 3)
+            return str;
+        var r = "";
+        for (var i = 0; i <= Math.floor(str.length / 3); i++) {
+            var idx = str.length - 3 * (i + 1), len = 3;
+            if (idx < 0) {
+                len += idx;
+                idx = 0;
+            }
+            if (len == 0)
+                break;
+            if (i > 0)
+                r = ',' + r;
+            r = str.substr(idx, len) + r
+        }
+        return r;
+    }
     //cookie
     function getCookie(c_name) {
         var c_value = document.cookie;
@@ -61,7 +80,7 @@
         $('#google_map .pattern i').not(':first').css('margin-left', iw - 20);
         $('#map_control').css('bottom', 30);
         $('#ui_board').css('left', iw * 7);
-        $('#nav').width(sw);
+        $('#main .copy').css('left', iw + 10);
         $('.col1').width(iw - 20);
         $('.col1-full').css('width', iw);
         $('#detail').css({
@@ -83,8 +102,10 @@
             'margin-right' : (iw - 20) - (iw - 20) / 2,
             width : (iw - 20) / 2
         });
-        $('#user_action .bg').css('top', (sh - $('#user_action .bg').height()) / 2);
-        $('#user_action .select p').css('left', $('#user_action .select a.email').position().left + $('#user_action .select a.email').width() - $('#user_action .select p').width() + 40);
+        if ($('#user_action').length > 0) {
+            $('#user_action .bg').css('top', (sh - $('#user_action .bg').height()) / 2);
+            $('#user_action .select p').css('left', $('#user_action .select a.email').position().left + $('#user_action .select a.email').width() - $('#user_action .select p').width() + 40);
+        }
         $('.col-square').each(function() {
             $(this).css('height', $(this).width());
         });
@@ -160,21 +181,37 @@
         return null;
     }
 
+    function dateFormat(date) {
+        var str = date.getFullYear() + '.';
+        str += (date.getMonth() + 1) + '.';
+        str += date.getDate()
+        return str;
+    }
+
+    function dayPass(date) {
+        var start = new Date(2013, 4, 24);
+        var span = date.getTime() - start.getTime();
+        return Math.ceil(span / 1000 / 3600 / 24);
+    }
+
     function markerClickHandle(e) {
-        var progress = new google.maps.Marker({
-            position : this.getPosition(),
-            clickable : false,
-            map : nsd.map,
-            icon : {
-                fillColor : '#e05206',
-                fillOpacity : 1,
-                strokeOpacity : 0,
-                path : getProgressSymbolPath(0),
-                anchor : new google.maps.Point(12, 12)
-            },
-            optimized : false,
-            zIndex : 1
-        });
+        var progress;
+        if (nsd.user.token != null) {
+            progress = new google.maps.Marker({
+                position : this.getPosition(),
+                clickable : false,
+                map : nsd.map,
+                icon : {
+                    fillColor : '#e05206',
+                    fillOpacity : 1,
+                    strokeOpacity : 0,
+                    path : getProgressSymbolPath(0),
+                    anchor : new google.maps.Point(12, 12)
+                },
+                optimized : false,
+                zIndex : 1
+            });
+        }
         $('#content').data('progress', {
             p : 0,
             origin : this,
@@ -197,30 +234,50 @@
             } else {
                 clearInterval(config.timer);
             }
-            console.log('auto', timer_sec);
         }, 500);
-        var loc = findMarkerFromList(this, markers);
+        var loc, loc_data;
+        var dist = 0;
+        for (var i = 0; i < markers.length; i++) {
+            dist += markers[i].distance;
+            if (this === markers[i].obj) {
+                loc = markers[i];
+                loc_data = nsd.data.location[i];
+                break;
+            }
+        }
+
+        $('#ui_info li').eq(0).children('span').html(loc_data.Location);
+        var currTime = new Date(loc_data.CurrentTime + "+0800");
+        $('#ui_info li').eq(1).children('b').html(dayPass(currTime));
+        $('#ui_info li').eq(1).children('span').html(dateFormat(currTime));
+        $('#ui_info li').eq(2).children('b').html(shared.formatNumber(dist));
+        $('#ui_info li').eq(3).children('span').html(loc_data.Latitude + ',' + loc_data.Longitude);
+        $('#ui_info li').eq(4).children('b').html(loc_data.PhotoCount);
+
+        shared.location = loc.id;
         $.post(config.api_path, {
             ac : "imgbyloc",
             loc : loc.id
         }, function(data) {
-            var item = nsd.data.location[loc.index];
-            $('#detail h2').html(item.Title);
-            $('#detail .content').html(item.RichContent);
-            $('#content').show();
-            $('.gallery-list ul').empty();
-            for (var i = 0; i < data.list.length; i++) {
-                var item = data.list[i];
-                var li = $('<li class="img-hover"/>').appendTo('.gallery-list ul');
-                var img = $('<img/>').attr({
-                    src : item.FileName + 't.jpg',
-                    alt : loc.Location
-                }).appendTo(li);
-                li.append('<span><a data-index="' + i + '"></a></span>');
+            if (generalErrorHandle(data)) {
+                var item = nsd.data.location[loc.index];
+                $('#detail h2').html(item.Title);
+                $('#detail .content').html(item.RichContent);
+                $('#content').show();
+                $('.gallery-list ul').empty();
+                for (var i = 0; i < data.list.length; i++) {
+                    var item = data.list[i];
+                    var li = $('<li class="img-hover"/>').appendTo('.gallery-list ul');
+                    var img = $('<img/>').attr({
+                        src : item.FileName + 't.jpg',
+                        alt : loc.Location
+                    }).appendTo(li);
+                    li.append('<span><a data-index="' + i + '"></a></span>');
+                }
+                initGallery(data, loc.Location);
+                setTimeout(adjust, 200);
+                //clearInterval(timer);
             }
-            initGallery(data, loc.Location);
-            setTimeout(adjust, 200);
-            //clearInterval(timer);
         });
     }
 
@@ -232,7 +289,7 @@
         var setSwitch = function() {
             var next_id = shared.gallery_id + 1, prev_id = shared.gallery_id - 1;
             if (next_id >= list.length)
-                next_id = 0
+                next_id = 0;
             if (prev_id < 0)
                 prev_id = list.length - 1;
             if (list.length == 2)
@@ -243,7 +300,7 @@
             $('#gallery').css('top', '100%');
             $('#ui_info').show();
         }, showGallery = function() {
-            var idx = Number($(this).attr('data-index'));
+            var idx = $(this).data('index');
             shared.gallery_id = idx;
             $('#gallery > .frame').eq(shared.gallery_id).addClass('visible');
             $('#gallery > .pager a').eq(shared.gallery_id).addClass('active');
@@ -253,7 +310,7 @@
         }, goFrame = function() {
             if ($(this).hasClass('active'))
                 return;
-            var idx = Number($(this).attr('data-index'));
+            var idx = $(this).data('index');
             if (!$.isNumeric(idx)) {
                 idx = $(this).index();
             }
@@ -302,6 +359,8 @@
         $('#content .gallery-list li a').click(showGallery);
         //hasLayout
         $('#gallery').css('top', '100%').show();
+        $('.gallery-list').removeClass('visible');
+        $('#content .mask,#content .right-button.back').hide();
         movePattern();
     }
 
@@ -360,7 +419,7 @@
         $.getJSON(config.api_path, {
             ac : "getlocation"
         }, function(data) {
-            if (data.code == 0) {
+            if (generalErrorHandle(data)) {
                 nsd.data.location = data.list;
 
                 for (var i = 0; i < data.list.length; i++) {
@@ -378,7 +437,8 @@
                         id : loc.ID,
                         index : i,
                         obj : marker,
-                        type : loc.Type
+                        type : loc.Type,
+                        distance : loc.Distance
                     });
                     if (loc.Type < 5) {
                         wayPoints.push({
@@ -387,7 +447,7 @@
                     }
                     if (loc.Status == 2) {
                         google.maps.event.addListener(marker, 'mouseover', function(e) {
-                            if (!$('#content').data('progress')) {
+                            if ($('#content').data('progress') == null) {
                                 this.setIcon({
                                     url : 'img/ol-hover.png',
                                     anchor : new google.maps.Point(70, 44)
@@ -482,29 +542,86 @@
         }
         connecting = true;
         form.find('button').addClass('loading');
-        $.post('api/action.aspx', form.serialize(), function(data) {
+        $.post(config.api_path, form.serialize(), function(data) {
             connecting = false;
             form.find('button').removeClass('loading');
-            if (data.code == 101) {
-                alert("该邮箱已被使用。");
-                $('input[name="email"]').select();
-                return;
-            } else if (data.code == 102) {
-                alert("邮箱或密码错误。");
-                return;
-            }
-            if (data.code == 0) {
-                setCookie("auth_token", data.token);
-                $('#user_action').hide();
+            if (generalErrorHandle(data)) {
+                userSignIn(data);
             }
         });
     }
 
-    function setWeiboUser(data) {
+    function userSignIn(data) {
+        if (data.token == null || data.token == "")
+            return false;
+        nsd.user.token = data.token;
         setCookie("auth_token", data.token);
-        //data.points
+        if (data.sinaid != null && data.sinaid != "" && data.sinaid != 0) {
+            setCookie("weibo_user", true);
+            nsd.user.weibo = true;
+        }
+        setUserPoints(data.points);
+        $('#ui_user .btns').hide();
+        $('#ui_user .bar').show();
+        $('#ui_user .btn-left').show();
         $('#user_action').hide();
-        $('.select,.login,.register').hide();
+    }
+
+    function userSignOut() {
+        nsd.user.token = null;
+        nsd.user.points = null;
+        nsd.user.weibo = false;
+        setCookie("auth_token", null, new Date());
+        setCookie("user_points", "", new Date());
+        setCookie("weibo_user", "", new Date());
+        $('#ui_user .btns').show();
+        $('#ui_user .bar').hide();
+        $('#ui_user .btn-left').hide();
+    }
+
+    function setWeiboUser(data) {
+        if (generalErrorHandle(data)) {
+            setCookie("weibo_user", true);
+            nsd.user.weibo = true;
+            userSignIn(data);
+        }
+    }
+
+    function setUserPoints(pt) {
+        nsd.user.points = pt;
+        setCookie("user_points", pt);
+        $('#ui_user .bar span b').text(shared.formatNumber(pt));
+    }
+
+    function openWeiboAuth() {
+        var win = window.open("OAuth.aspx?auth=1", "auth_weibo", "width=615,height=505");
+    }
+
+    function generalErrorHandle(data) {
+        if (data.code == 0)
+            return true;
+        if (data.code == 101) {
+            alert("该邮箱已被使用。");
+            $('input[name="email"]').select();
+        } else if (data.code == 102) {
+            alert("邮箱或密码错误。");
+        } else if (data.code === 110) {
+            alert("请先登录。");
+            userSignOut();
+        } else if (data.code === 111) {
+            alert("请先登录。");
+            userSignOut();
+        } else if (data.code == 112) {
+            alert("微博账号已绑定到其它账号，请直接使用微博账号登录。");
+        } else if (data.code == 113) {
+            alert("请先绑定你的微博账号。");
+            openWeiboAuth();
+        } else if (data.code == 114) {
+            //alert("你已经访问过这个地方了。");
+        } else {
+            alert("发生错误，请重试。");
+        }
+        return false;
     }
 
     function delegateListener() {
@@ -518,7 +635,7 @@
         });
         $('#detail').scroll(function() {
             var progress = $('#content').data('progress');
-            if (progress) {
+            if (progress && progress.obj) {
                 var tp = $(this).scrollTop();
                 var max = $('#detail .article').height() - $(this).height();
                 var p = tp / max;
@@ -530,14 +647,38 @@
                     progress.p = p;
                 }
                 if (p >= 1) {
-                    shared.animation.flash(progress.obj);
                     progress.p = 100;
+                    //add point
+                    if (nsd.user.token != null) {
+                        $.post(config.api_path, {
+                            ac : 'followroute',
+                            token : nsd.user.token,
+                            loc : shared.location
+                        }, function(data) {
+                            if (generalErrorHandle(data)) {
+                                shared.animation.flash(progress.obj);
+                                setUserPoints(data.points);
+                                alert("恭喜您通过浏览行程获得了 " + data.amount + " 点积分。");
+                                progress.obj.setMap();
+                            }
+                        });
+                    }
                 }
             }
         });
-        $('#content .right-button.browse a').click(function() {
+        $('#content .right-button a.browse').click(function() {
             $('.gallery-list').addClass('visible');
             $('#content .mask,#content .right-button.back').show();
+        });
+        $('#content .right-button a.close').click(function() {
+            var mk = $('#content').data('progress');
+            google.maps.event.addListenerOnce(mk.origin, 'click', markerClickHandle);
+            if (mk.obj)
+                mk.obj.setMap();
+            nsd.map.setOptions({
+                draggable : true
+            });
+            $('#content').data('progress', null).hide();
         });
         $('#content .right-button.back a').click(function() {
             $('.gallery-list').removeClass('visible');
@@ -562,21 +703,25 @@
                     alert("请先登录。");
                     return;
                 }
-                $('#gallery .share textarea').val(status + " " + window.location.href);
+                if (!nsd.user.weibo) {
+                    alert("请先绑定一个微博账号。");
+                    openWeiboAuth();
+                    return;
+                }
                 connecting = true;
+                $('input[name="pic"]').val($('#gallery .switch a').eq(shared.gallery_id).children().attr('src'));
+                $('input[name="token"]').val(nsd.user.token);
+                $('input[name="url"]').val(window.location.href);
                 $.post(config.api_path, $('#gallery .share form').serialize(), function(data) {
                     connecting = false;
-                    if (data.code == 0) {
+                    if (generalErrorHandle(data)) {
+                        setUserPoints(data.points);
+                        alert("分享成功！\n恭喜您获得了 " + data.amount + " 点积分。");
                         $('#gallery .share form').removeClass('bg');
                         $('#gallery .share').css('height', '');
                         $('#gallery .mask').hide();
                         $('#gallery .share p.submit span').width(0);
                         $('#gallery .share p.submit').removeClass('submit');
-                    } else if (data.code == 111) {
-                        alert("请先绑定你的微博账号。");
-                        window.open("OAuth.aspx", "sina_oauth", "");
-                    } else {
-                        alert(data.msg)
                     }
                 });
             } else {
@@ -589,8 +734,7 @@
                     left : pos.left + $('#gallery textarea').width() - $('#gallery .btn-cancel').width() + 10,
                     top : pos.top + $('#gallery textarea').height() - $('#gallery .btn-cancel').height() + 5
                 })
-                $('input[name="pic"]').val($('#gallery .switch a').eq(shared.gallery_id).children().attr('src'));
-                $('#gallery .share textarea').focus();
+                $('#gallery textarea').focus();
             }
         });
         $('#gallery .share .btn-cancel').click(function() {
@@ -600,8 +744,11 @@
             $('#gallery .share p.submit span').width(0);
             $('#gallery .share p.submit').removeClass('submit');
         });
-        $('#ui_user a').click(function() {
+        $('#ui_user .btns a').click(function() {
             var idx = $(this).index();
+            if ($(this).parent().hasClass('btn-left')) {
+                return;
+            }
             $('#user_action').show();
             if (idx == 0) {
                 $('#user_action .pattern').hide();
@@ -614,8 +761,9 @@
             }
             adjust();
         });
+        $('#ui_user a.exit').click(userSignOut);
         $('#user_action a.weibo').click(function() {
-            window.open("OAuth.aspx", "sina_oauth", "");
+            openWeiboAuth();
         });
         $('#user_action a.email').click(function() {
             $('#user_action').addClass('wide');
@@ -655,11 +803,24 @@
         });
         $('#user_action button').click(submitUser);
         $('#user_action input.textbox').keypress(submitUser);
+        $('#about .tabs li').click(function() {
+            if ($(this).hasClass('active'))
+                return;
+            var idx = $(this).index();
+            $('#about .tab-item').hide().eq(idx).show();
+            $('#about .tabs li.active').removeClass('active');
+            $(this).addClass('active');
+        });
     }
 
     $(function() {
         nsd.data = {};
-        nsd.user.token = getCookie("auth_token");
+        var weibo = getCookie("weibo_user");
+        nsd.user.weibo = weibo || false;
+        userSignIn({
+            token : getCookie("auth_token"),
+            points : Number(getCookie("user_points"))
+        });
         adjust();
         $(window).resize(adjust);
         initMap();
